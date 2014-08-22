cab3f8a93d50a359da1cfc402063c0861008a850
Kanstantsin <Kanstantsin_Makarau@epam.com>
2014-08-22 17:07:56 +0300

    edit verifier

--- src/main/java/com/epam/airline/logic/Verifier.java
+++ src/main/java/com/epam/airline/logic/Verifier.java
@@ -1,21 +1,32 @@
 package com.epam.airline.logic;
 
 import com.epam.airline.entity.Airliner;
+import com.epam.airline.entity.Freighter;
 import com.epam.airline.entity.Plane;
 
 /**
  * Created by Kanstantsin_Makarau on 8/21/2014.
  */
 public class Verifier {
-    //todo
+
     public static boolean checkPlane(Plane plane){
-        Airliner airliner = null;
+        if(plane.getName() == "" || plane.getRange() <= 0 || plane.getFuelConsumption() <= 0){
+            return false;
+        }
+
         if(plane instanceof Airliner){
-            airliner = (Airliner)plane;
-            if(airliner.getName() == ""){
+            Airliner airliner = (Airliner)plane;
+            if(airliner.getSeats() <= 0){
                 return false;
             }
         }
+        if(plane instanceof Freighter){
+            Freighter freighter = (Freighter)plane;
+            if(freighter.getCapacity() <= 0){
+                return false;
+            }
+        }
+
         return true;
     }
 }
